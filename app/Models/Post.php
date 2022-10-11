9331ab49396ae488c65d59547cd3ff814e2b0a46
isaac <isaacd4444@gmail.com>
2022-10-11 19:26:41 -0400

    shows posts from all users

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+
+        'message',
+
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2022_10_11_222900_create_posts_table.php
+++ database/migrations/2022_10_11_222900_create_posts_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->string('message');
+
+           
         });
     }
 
