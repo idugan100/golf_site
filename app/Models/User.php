001b28ea51a2d30825b689b0f923be98183686ac
isaac <isaacd4444@gmail.com>
2022-10-11 19:04:17 -0400

    defined user->posts relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Post;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    public function posts()
+
+    {
+
+        return $this->hasMany(Post::class);
+
+    }
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
