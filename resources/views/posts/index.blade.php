c57149d0a866ceeda86c7cb8e932cd2a753f2c62
isaac <isaacd4444@gmail.com>
2022-10-12 13:54:12 -0400

    comments about next steps in posts.index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,6 +11,7 @@
             <x-primary-button class="mt-4">{{ __('Post') }}</x-primary-button>
         </form>
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+             {{-- Lets refactor this to make it cleaner --}}
             @foreach ($posts as $post)
                 <div class="p-6 flex space-x-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -58,6 +59,8 @@
                         </div>
                         <p class="mt-4 text-lg text-gray-900">{{ $post->message }}</p>
                     </div>
+                    {{-- lets create the comment accordian button element here
+                        with comments by latest with the comments.store form at the top --}}
                 </div>
             @endforeach
         </div>
