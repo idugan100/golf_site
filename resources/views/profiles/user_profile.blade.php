f76e66abfb74087b6811d1233254b3cb4492777a
isaac <isaacd4444@gmail.com>
2022-10-21 11:36:41 -0400

    styled profile edit screen

--- resources/views/profiles/user_profile.blade.php
+++ resources/views/profiles/user_profile.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
     <div class="flex flex-col items-center justify-center mt-10">
         <h1 class="text-3xl font-bold">Your Profile Info</h1>
-        <img class="h-64 mt-10 rounded"src="{{ asset('storage/'.auth()->user()->profile_picture) }}" alt="golf photo">
+        <img class="h-64 mt-10 rounded"src="{{ asset('storage/'.auth()->user()->profile_picture) }}" alt="missing profile photo">
         <h2 class="text-lg underline mt-10">{{ "About ". auth()->user()->name }}</h2>
         <p>{{ auth()->user()->about }}</p>
         <h2 class="text-lg underline mt-10">{{auth()->user()->name."'s Favorite Golf Course" }}</h2>
--- resources/views/profiles/user_update.blade.php
+++ resources/views/profiles/user_update.blade.php
@@ -1,19 +1,29 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <h1>edit screen</h1>
+        <h1 class="text-3xl font-bold">Edit Profile</h1>
         <form method="POST"  action="/profile/update" enctype="multipart/form-data">
             @csrf
             @method("PATCH")
-           <div>
             <label>About Me</label>
-            <input type="text" name="about">
-           </div>
-           <div>
+            
+            <div>
+            <textarea type="text" name="about">{{ old('about', auth()->user()->about) }}</textarea>
+            </div>
             <label>Favorite Golf Course</label>
-            <input type="text" name="favorite_golf_course">
+            <div>
+            
+            <textarea type="text" name="favorite_golf_course">{{ old('about', auth()->user()->favorite_golf_course) }}</textarea>
            </div>
+           <label>Profile Photo</label>
+           @if (auth()->user()->profile_picture)
+                <div class="my-4">
+                    <img
+                    class=" w-48 mr-6 md:block"
+                    src= "{{asset('storage/'.auth()->user()->profile_picture )  }}" />
+                </div>
+            @endif
            <div>
-            <label>Profile Photo</label>
+            
             <input type="file" name="profile_picture">
            </div>
             
