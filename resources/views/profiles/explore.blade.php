019403e877e767363f8512893af7ee5925451751
isaac <isaacd4444@gmail.com>
2022-10-28 15:57:05 -0400

    exmplore page simple styling

--- resources/views/profiles/explore.blade.php
+++ resources/views/profiles/explore.blade.php
@@ -1,7 +1,16 @@
+<x-app-layout>
+    <div class="p-2 max-w-2xl mx-auto">
 <h1>explore</h1>
+
 @foreach ($profiles as $profile)
-<div>
-    {{ $profile->name }}
+<div class="bg-blue-800 text-white p-2 m-2 font-bold border">
+    {{ $profile->name }}  
+    <button class="border p-1 ml-4">visit 
+    </button>
 </div>
+
+
     
 @endforeach
+</div>
+</x-app-layout>
