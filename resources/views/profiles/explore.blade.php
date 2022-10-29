68dccfe95fa013db7ec005ee22b4b68f5aa96458
isaac <isaacd4444@gmail.com>
2022-10-29 11:13:03 -0400

    visit buttons take you to the persons profile page

--- resources/views/profiles/explore.blade.php
+++ resources/views/profiles/explore.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
-    <div class="p-2 max-w-2xl mx-auto">
-<h1>explore</h1>
+<div class="p-2 max-w-2xl mx-auto">
+<h1 class="text-3xl font-bold">People</h1>
 
 @foreach ($profiles as $profile)
 <div class="bg-blue-800 text-white p-2 m-2 font-bold border">
     {{ $profile->name }}  
-    <button class="border p-1 ml-4">visit 
+    
+    <button class="border p-1 ml-4"><a href= "/profile/{{$profile->id  }}">Visit</a> 
     </button>
 </div>
 
