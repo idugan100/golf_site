d089343d7588c9aba507034b6fce352fa57d118c
isaac <isaacd4444@gmail.com>
2022-11-05 11:36:32 -0400

    add freind list on profile

--- resources/views/profiles/user_profile.blade.php
+++ resources/views/profiles/user_profile.blade.php
@@ -1,4 +1,5 @@
 
+
 <x-app-layout>
     <div class="flex flex-col items-center justify-center mt-10">
         <h1 class="text-3xl font-bold">Your Profile Info</h1>
@@ -8,5 +9,13 @@
         <h2 class="text-lg underline mt-10">{{auth()->user()->name."'s Favorite Golf Course" }}</h2>
         <p>{{ auth()->user()->favorite_golf_course }}</p>
         <a href="{{ route('profile.edit') }}" class=" mt-10 bg-blue-500 rounded p-1">Edit</a>
+     <h1 class="text-xl font-bold mt-10 underline">Friends</h1>
+       
+    @foreach ($friends as $item)
+        <p>{{ $item->name }}</p>
+        
+    @endforeach
+  
     </div>
+    
 </x-app-layout>
